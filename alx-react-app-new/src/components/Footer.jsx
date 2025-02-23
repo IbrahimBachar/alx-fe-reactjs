37ca4325303bb60fe3572420dfb7a5fced9ffe16
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-02-23 11:32:17 +0200

    inline styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div style={{backgroundColor: 'gray', color: 'white', textAlign: 'center'}}>
+      <footer>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+    <div>
+      <main style={{textAlign: 'center', fontSize: '20px', color: 'rebeccapurple', padding: '20px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    </div>
+  )
+}
+
+export default MainContent
