ca549b7ee7ec16bd6a1ca8559e0a2223f38e067b
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-03-03 00:09:02 +0200

    recipe sharing app with zustand

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,29 +2,9 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  searchTerm: '',
-  
-  setSearchTerm: (term) => set({ searchTerm: term }),
-
-  filteredRecipes: (state) =>
-    state.recipes.filter((recipe) =>
-      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-    ),
-
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
-
-  updateRecipe: (id, updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
-      ),
-    })),
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
 export default useRecipeStore;
