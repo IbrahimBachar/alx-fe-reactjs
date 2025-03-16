a6a3f80420c3d5f89c8cb31da53b76e075b5cf1a
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-03-16 17:40:46 +0200

    Advanced routing in react with React Router Updated

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,10 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
-
-const isAuthenticated = () => {
-  return localStorage.getItem("auth") === "true";
-};
+import {useAuth} from "../hooks/useAuth";
 
 const ProtectedRoute = () => {
+    const isAuthenticated = useAuth();
   return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
 };
 
